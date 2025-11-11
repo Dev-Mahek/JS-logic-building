cfca2a2e474a6f6415b46e47660cdb56119f4aa3
Mahek Roy <dev-mahek@zohomail.in>
2025-11-11 21:28:20 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -11,3 +11,21 @@ for (let i = 1; i <= 20; i++) {
     console.log(i);
   }
 }
+
+// --------------Print odd numbers between 1 to 20----------------
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
+
+// -------------Find the sum of numbers from 1 to 100---------------
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+  sum = sum + i;
+}
+
+console.log(sum);
+
