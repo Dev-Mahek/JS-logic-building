c61610010fc0a6f39e772951e95be9e1ebcc39c5
Mahek Roy <dev-mahek@zohomail.in>
2025-11-11 21:26:11 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -4,3 +4,10 @@ for (let i = 1; i <= 10; i++) {
   console.log(i);
 }
 
+//-----------------Print even numbers between 1 to 20---------------------
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
